6527e082514cedb7159dd18e4650ecd3a6cd0f1b
besu178 <besuwinner@gmail.com>
2025-06-27 13:14:59 +0300

    end of chapter 8 login and logout functionality worked

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "wellcome to the sample app"
+      flash[:notice] = "wellcome to the social app"
       redirect_to @user
     else
       render "new", status: :unprocessable_entity
