e2b37258af39878f5a465b03fd5bfdaaa1a86704
besu178 <besuwinner@gmail.com>
2025-06-27 10:02:12 +0300

    finished chapter 6 of the book

--- db/migrate/20250620023014_create_users.rb
+++ db/migrate/20250620023014_create_users.rb
@@ -2,7 +2,8 @@ class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
+      t.string :email, null: false
+      add_index :users, :email, unique: true
 
       t.timestamps
     end
