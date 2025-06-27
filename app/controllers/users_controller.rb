e58722f235118b0132f91c1b0f1508bc582b20d5
besu178 <besuwinner@gmail.com>
2025-06-27 10:33:39 +0300

    ende of chapter 7 user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def show
+    User.find(params[:id])
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "wellcome to the sample app"
+      redirect_to @user
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+  private
+  def user_params
+    params.requre(:user).permit(:name, :email)
+  end
+end
